24bffaab17e2a1e9ef1b8b274c8f33e7744eb765
Fabian Ferno <fabianferno@gmail.com>
2021-02-21 01:59:29 +0530

    Gen config 🙌

--- db_config.py
+++ db_config.py
@@ -4,8 +4,8 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 
 # MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = "root"
+app.config['MYSQL_DATABASE_USER'] = ""
 app.config['MYSQL_DATABASE_PASSWORD'] = ""
-app.config['MYSQL_DATABASE_DB'] = "employees"
-app.config['MYSQL_DATABASE_HOST'] = "127.0.0.1"
+app.config['MYSQL_DATABASE_DB'] = ""
+app.config['MYSQL_DATABASE_HOST'] = ""
 mysql.init_app(app)
